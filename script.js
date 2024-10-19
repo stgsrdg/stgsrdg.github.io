b46d9cf242a829d8cb0c710653f978a1b3b965c0
stgsrdg <tupediatraestela@outlook.com>
2024-10-19 10:29:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-// Inicializar Swiper con desplazamiento horizontal para escritorio
-const swiper = new Swiper('.swiper-container', {
-    loop: false, // No repetir automáticamente
-    slidesPerView: 'auto', // Mostrar las imágenes en fila
-    spaceBetween: 10, // Espacio entre imágenes
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-});
-
-// Controlar el desplazamiento en dispositivos móviles
-function adjustScroll() {
-    if (window.innerWidth < 768) { // Solo aplicar en dispositivos móviles
-        window.scrollTo({
-            top: window.scrollY + window.innerHeight * 2, // Desplazar el doble de la altura de la pantalla
-            behavior: 'smooth'
-        });
-    }
-}
-
-window.addEventListener('scroll', adjustScroll);
-
-
-
-
 window.addEventListener("load", function() {
     const loader = document.getElementById("loader");
     const wrapper = document.getElementById("loader-wrapper");
