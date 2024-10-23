4e02f6dc4be2883485c160073b70b692ecb4b627
CodeMate <121621804+Clashroyalo5@users.noreply.github.com>
2024-10-23 15:19:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,15 @@ window.addEventListener("load", function() {
     // Desactiva la animación infinita cuando la página está completamente cargada
     loader.style.animationIterationCount = "1";
 });
+
+let currentIndex = 0;
+const slides = document.querySelectorAll('.carousel ul li');
+const totalSlides = slides.length;
+
+function moveSlide() {
+    currentIndex = (currentIndex + 1) % totalSlides;
+    const translateValue = -currentIndex * 100 + '%';
+    document.querySelector('.carousel ul').style.transform = `translateX(${translateValue})`;
+}
+
+setInterval(moveSlide, 3000); // Cambia de imagen cada 4,5 segundos
